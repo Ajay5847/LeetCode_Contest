c3bb5bd9582df46ddafc2feb1144fe9b809d029a
Ajay Kumar M <ajaykumarm.be23@uceou.edu>
2023-08-16 23:05:52 +0530

    Sliding Window Maximum

---
+++ DSA/Sliding Window/Sliding Window Maximum.java
@@ -0,0 +1,61 @@
+// You are given an array of integers nums,
+// there is a sliding window of size k which is moving from the very left of the array to the very right.
+// You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+// Example 1:
+
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation: 
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// 1 <= k <= nums.length
+
+//Intution
+
+// BruteForce will give TLE. So, we use an ArrayDeque where we will add the elements in the deque in a pattern as
+// first we will check the boundary condition whether the current index lies in the specified window or not
+// if it doesnt lie then we will poll from first q.poll(),
+// then if the current element is less than the previous element then we will poll it out from last.
+// then we will check the current max which will be present at the first
+// as we reach the specified window length then we will add the max element to the list
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> q = new ArrayDeque<>();
+        int idx = 0;
+        for(int i = 0;i < n;i++){
+            if(!q.isEmpty() && q.peek() == i - k){
+                q.poll();
+            }
+            while(!q.isEmpty() && nums[q.peekLast()] < nums[i]){
+                q.pollLast();
+            }
+            q.add(i);
+            if(i >= k - 1){
+                ans[idx++] = nums[q.peek()];
+            }
+        }
+        return ans;
+    }
+}

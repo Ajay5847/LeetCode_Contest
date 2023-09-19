7b474cb09d77606deea992f1fa2a315322e85de6
Ajay5847 <93735050+Ajay5847@users.noreply.github.com>
2023-09-19 23:40:00 +0530

    Update Sliding Window Maximum.java

--- DSA/Sliding Window/Sliding Window Maximum.java
+++ DSA/Sliding Window/Sliding Window Maximum.java
@@ -1,11 +1,9 @@
 // You are given an array of integers nums,
 // there is a sliding window of size k which is moving from the very left of the array to the very right.
 // You can only see the k numbers in the window. Each time the sliding window moves right by one position.
-
 // Return the max sliding window.
 
 // Example 1:
-
 // Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
 // Output: [3,3,5,5,6,7]
 
@@ -20,18 +18,15 @@
 //  1  3  -1  -3  5 [3  6  7]      7
 
 // Example 2:
-
 // Input: nums = [1], k = 1
 // Output: [1]
 
 // Constraints:
-
 // 1 <= nums.length <= 105
 // -104 <= nums[i] <= 104
 // 1 <= k <= nums.length
 
 //Intution
-
 // BruteForce will give TLE. So, we use an ArrayDeque where we will add the elements in the deque in a pattern as
 // first we will check the boundary condition whether the current index lies in the specified window or not
 // if it doesnt lie then we will poll from first q.poll(),
